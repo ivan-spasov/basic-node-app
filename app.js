905fe87b236296422c9b864b5103457ea90e9016
ivan-spasov <49478767+ivan-spasov@users.noreply.github.com>
2021-12-14 20:16:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const os = require('os');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req,res) => {
     res.send(`Hello ${os.hostname()}`);
